fecd5335996fda86085b2aa10c63e2ff7b56425e
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-10-02 17:36:05 +0600

    New

--- routes/test.route.js
+++ routes/test.route.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const loadUser = JSON.parse(readFileSync("data.json"));
 const route = express.Router();
 const { readFileSync, writeFileSync } = require("fs");
-
+const loadUser = JSON.parse(readFileSync("data.json"));
 
 route.get("/all", (req, res) => {
   res.send(loadUser);
@@ -29,13 +28,17 @@ route.post("/save", (req, res) => {
 });
 route.put("/all", (req, res) => {
   loadUser.push(req.body);
-  res.send(loadUser);
+  const putData = JSON.stringify(loadUser, null, 2);
+  const putUser = writeFileSync("data.json", putData);
+  res.send(putUser);
 });
 
 route.delete("/all/:id", (req, res) => {
   const { id } = req.params;
   const foundData = loadUser.find((loadUser) => loadUser.id == id);
-  res.send(foundData);
+  const deleteData = JSON.stringify(loadUser, null, 2);
+  const deleteUser = writeFileSync("data.json", foundData);
+  res.send(deleteUser);
 });
 
 module.exports = route;
