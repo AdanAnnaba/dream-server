aeb3ca8781b67ba0f172b435abc10a3d111d76fe
Nahid Ahmmed Likhon <lemonnahid19@gmail.com.com>
2022-12-19 22:05:36 +0600

    Some Correction

--- routes/test.route.js
+++ routes/test.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const { readFileSync, writeFileSync } = require("fs");
-const loadUser = JSON.parse(readFileSync("\data.json"));
+const loadUser = JSON.parse(readFileSync("data.json"));
 
 route.get("/all", (req, res) => {
   res.send(loadUser);
@@ -11,9 +11,8 @@ route.get("/random", (req, res) => {
   res.send(random);
 });
 
-route.get("all/:id", (req, res) => {
-  const id  = req.params;
-  console.log(id);
+route.get("/all/:id", (req, res) => {
+  const {id}  = req.params;
   const foundData = loadUser.find((loadUser) => loadUser.id == id);
   res.send(foundData);
 });
@@ -21,21 +20,21 @@ route.post("/save", (req, res) => {
   const data = req.body;
   loadUser.push(data);
   const newData = JSON.stringify(loadUser, null, 2);
-  const saveUser = writeFileSync("\data.json", newData);
+  const saveUser = writeFileSync("data.json", newData);
   res.send(saveUser);
 });
 route.put("/all:id", (req, res) => {
   loadUser.push(req.body);
   const putData = JSON.stringify(loadUser, null, 2);
-  const putUser = writeFileSync("\data.json", putData);
+  const putUser = writeFileSync("data.json", putData);
   res.send(putUser);
 });
 
 route.delete("/all/:id", (req, res) => {
   const { id } = req.params;
-  const foundData = loadUser.find((loadUser) => loadUser.id == id);
-  const deleteData = JSON.stringify(loadUser, null, 2);
-  const deleteUser = writeFileSync("\data.json", foundData);
+  const foundData = loadUser.find((loadUser) => loadUser.id != id);
+  const deleteData = JSON.stringify(foundData, null, 2);
+  const deleteUser = writeFileSync("data.json", deleteData);
   res.send(deleteUser);
 });
 
